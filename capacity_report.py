e23d7bfce86393a5117e3aee4c74f08f6dfb0526
MD. MAHFUZUR RAHMAN <163427998+mahfuz-31@users.noreply.github.com>
2025-07-01 17:20:11 +0600

    Refactor code structure for improved readability and maintainability

--- capacity_report.py
+++ capacity_report.py
@@ -7,16 +7,16 @@ from openpyxl.styles import Font # type: ignore
 
 
 yes = input("Enter Yesterday Folder Name: ")
-yesLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/06. Jun/" + str(yes) + "/"
+yesLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/07. Jul/" + str(yes) + "/"
 
 today = input("Enter Today's Folder Name: ")
-todLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/06. Jun/" + str(today) + "/"
+todLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/07. Jul/" + str(today) + "/"
 
-cur_month = 'Jun'
-plan_month = 'Jul'
+cur_month = 'Jul'
+plan_month = 'Aug'
 plan_month_end = '31'
-plan_next_month = 'Aug'
-plan_next_month_end = '31'
+plan_next_month = 'Sep'
+plan_next_month_end = '30'
 
 yes_buyer = pd.read_csv(yesLocation + "Buyer wise monthly plan qty.csv")
 tod_buyer = pd.read_csv(todLocation + "Buyer wise monthly plan qty.csv")
@@ -112,10 +112,10 @@ result_unit[tod_second_t] = tod_unit[tod_unit_cols[3]]
 result_unit[yes_second_t] = yes_unit[yes_unit_cols[3]]
 result_unit[change_second_t] = result_unit[tod_second_t] - result_unit[yes_second_t]
 
-first_w_days = 27
+first_w_days = 26
 second_w_days = 26
 
-first_blank_days = first_w_days * 415
+first_blank_days = first_w_days * 427
 second_blank_days = second_w_days * 427
 
 first_capacity_pctg = (result_unit.iloc[7, 2] / first_blank_days) * 100
@@ -497,7 +497,7 @@ for row_idx, row in enumerate(ws_unit_and_buyer_range, start=start_row):
             destination_cell.number_format = cell.number_format
 
 curr_month_plan_qt_cell = ws_unit['A12']
-curr_month_plan_qt_cell.value = 'May Plan Quantity ='
+curr_month_plan_qt_cell.value = 'Jul Plan Quantity ='
 curr_month_plan_qt_cell.font = Font(name='Arial', bold=True)
 ws_unit.merge_cells('A12:B12')
 
@@ -516,7 +516,7 @@ ws_unit.merge_cells('A1:I1')
 ws_unit['A1'].alignment = Alignment(horizontal='center', vertical='center')
 
 wb.save(outputFile)
-outputFile2 = '//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/Reports/06. Jun/' + str(today_date) + '.xlsx'
+outputFile2 = '//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/Reports/07. Jul/' + str(today_date) + '.xlsx'
 wb.save(outputFile2)
 
 print("\nSuccessfully done :)")
