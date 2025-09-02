de83740b75e11e0008cca8a8d5f3e443f65a9230
mahfuz-31 <sm.mahfuz031@gmail.com>
2025-09-02 09:48:44 +0600

    updated for sep month

--- capacity_report.py
+++ capacity_report.py
@@ -7,16 +7,16 @@ from openpyxl.styles import Font # type: ignore
 
 
 yes = input("Enter Yesterday Folder Name: ")
-yesLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/08. Aug/" + str(yes) + "/"
+yesLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/09. Sep/" + str(yes) + "/"
 
 today = input("Enter Today's Folder Name: ")
-todLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/08. Aug/" + str(today) + "/"
+todLocation = "//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/09. Sep/" + str(today) + "/"
 
-cur_month = 'Aug'
-plan_month = 'Sep'
-plan_month_end = '30'
-plan_next_month = 'Oct'
-plan_next_month_end = '31'
+cur_month = 'Sep'
+plan_month = 'Oct'
+plan_month_end = '31'
+plan_next_month = 'Nov'
+plan_next_month_end = '30'
 
 yes_buyer = pd.read_csv(yesLocation + "Buyer wise monthly plan qty.csv")
 tod_buyer = pd.read_csv(todLocation + "Buyer wise monthly plan qty.csv")
@@ -516,7 +516,7 @@ ws_unit.merge_cells('A1:I1')
 ws_unit['A1'].alignment = Alignment(horizontal='center', vertical='center')
 
 wb.save(outputFile)
-outputFile2 = '//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/Reports/08. Aug/' + str(today_date) + '.xlsx'
+outputFile2 = '//192.168.1.231/Planning Internal/Capacity planning/Capacity Report/2025/Reports/09. Sep/' + str(today_date) + '.xlsx'
 wb.save(outputFile2)
 
 print("\nSuccessfully done :)")
